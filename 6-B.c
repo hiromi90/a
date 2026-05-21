b233ffa4062817fd84afc7f279a7c26c17d14fdb
hiromi90 <x24104xx@aitech.ac.jp>
2026-05-21 23:21:40 +0900

    a

--- 6-B.c
+++ 6-B.c
@@ -5,8 +5,9 @@ int main(int argc,const char * argv[])
     int n;
     scanf("%d",&n);
     int a[n],S[13],H[13],C[13],D[13];
-    char s[n];
+    char s[n],ss[n];
     for(int i=0;i<n;i++){
+        scanf("%c",&ss[i]);
         scanf("%c %d",&s[i],&a[i]);
     }
     for(int i=0;i<13;i++){
@@ -43,30 +44,27 @@ int main(int argc,const char * argv[])
         }
     }
 
-    printf("%d\n\n",n);
-    for(int i=0;i<n;i++){
-        printf("%c %d\n",s[i],a[i]);
-    }
     
-    // for(int i=1;i<14;i++){
-    //     if(S[i-1]==0){
-    //         printf("S %d\n",i);
-    //     }
-    // }
-    // for(int i=1;i<14;i++){
-    //     if(H[i-1]==0){
-    //         printf("H %d\n",i);
-    //     }
-    // }
-    // for(int i=1;i<14;i++){
-    //     if(C[i-1]==0){
-    //         printf("C %d\n",i);
-    //     }
-    // }
-    // for(int i=1;i<14;i++){
-    //     if(D[i-1]==0){
-    //         printf("D %d\n",i);
-    //     }
-    // }
+    
+    for(int i=1;i<14;i++){
+        if(S[i-1]==0){
+            printf("S %d\n",i);
+        }
+    }
+    for(int i=1;i<14;i++){
+        if(H[i-1]==0){
+            printf("H %d\n",i);
+        }
+    }
+    for(int i=1;i<14;i++){
+        if(C[i-1]==0){
+            printf("C %d\n",i);
+        }
+    }
+    for(int i=1;i<14;i++){
+        if(D[i-1]==0){
+            printf("D %d\n",i);
+        }
+    }
     return 0;
 }
