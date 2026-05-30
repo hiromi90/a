323afbdd78aca9cb6ffb9e0e87eddeec65b5cf8d
hiromi90 <x24104xx@aitech.ac.jp>
2026-05-30 13:23:37 +0900

    a

--- 7-C.c
+++ 7-C.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 
+int main(){
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int a[r][c];
+    int sumr=0;
+    int sumc[c];
+    int sum=0;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(int i=0;i<c;i++){
+        sumc[i]=0;
+    }
+
+    for(int i=0;i<r;i++){
+        sumr=0;
+        for(int j=0;j<c;j++){
+            if(j!=c){
+                printf("%d ",a[i][j]);
+            }
+            sumr=sumr+a[i][j];
+            if(j==c-1){
+                printf("%d\n",sumr);
+            }
+            sumc[j]=sumc[j]+a[i][j];
+        }
+    }
+
+    for(int i=0;i<c;i++){
+        printf("%d ",sumc[i]);
+        sum=sum+sumc[i];
+    }
+
+    printf("%d\n",sum);
+
+}
