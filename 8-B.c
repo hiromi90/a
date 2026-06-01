80462bc058c5737a98cb660186c0214989599da7
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-01 19:43:17 +0900

    a

--- 8-B.c
+++ 8-B.c
@@ -2,5 +2,18 @@
 
 int main()
 {
-    
+    char x[1001];
+    int sum;
+    while(1){
+        sum=0;
+        scanf("%s",x);
+        if(x[0]=='0'&&x[1]=='\0'){
+            break;
+        }
+        for(int i=0;x[i]!='\0';i++){
+            sum=sum+(x[i]-'0');
+        }
+        printf("%d\n",sum);
+    }
+    return 0;
 }
