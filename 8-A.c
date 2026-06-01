94a4a1ffe2dc6a1d5092755f7eb9470084e1b7c7
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-01 19:17:48 +0900

    a

--- 8-A.c
+++ 8-A.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    
+    char s[1200];
+    scanf("%s",s);
+
+    for(int i=0;i<1200;i++){
+        if(s[i]>=65&&s[i]<=90){
+            s[i]=s[i]+32;
+        }
+        else if(s[i]>=97&&s[i]<=122){
+            s[i]=s[i]-32;
+        }
+        else{
+            s[i]=s[i];
+        }
+    }
+    printf("%s",s);
 }
