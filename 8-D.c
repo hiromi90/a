7ea65a541c72979b83b3773dd9ae680ba6d9405d
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-01 20:31:43 +0900

    a

--- 8-D.c
+++ 8-D.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    
+    char s[101],p[101];
+
+    scanf("%s",s);
+    scanf("%s",p);
+
+    char temp[202];
+
+    strcpy(temp, s);
+    strcat(temp,s);
+
+    if(strstr(temp,p)==NULL){
+        printf("No\n");
+    }
+    else{
+        printf("Yes\n");
+    }
+    return 0;
 }
