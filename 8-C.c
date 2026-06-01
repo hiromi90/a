3dca7d1b5152aed0eacb642f21a20837338d895f
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-01 20:00:47 +0900

    a

--- 8-C.c
+++ 8-C.c
@@ -2,5 +2,17 @@
 
 int main()
 {
-    
+    int sum[26] = {0};
+    char s;
+    while(scanf("%c",&s)!=EOF){
+        for(int i=65;i<=90;i++){
+            if(s==i||s==i+32){
+                sum[i-65]++;
+            }
+        }
+    }
+    for(int i=0;i<26;i++){
+        printf("%c : %d\n",i+97,sum[i]);
+    }
+    return 0;
 }
