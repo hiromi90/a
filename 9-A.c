4990680638ae6633422260d425c9b1dd3f09ea27
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-02 00:53:54 +0900

    a

--- 9-A.c
+++ 9-A.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    
+    char w[11];
+    scanf("%s", w);
+    char t[1000];
+    int sum=0;
+
+    while(1){
+        scanf("%s",t);
+        int len = strlen(t);
+        if(strcmp(t,"END_OF_TEXT") == 0) break;
+        for(int i=0;i<len;i++){
+            t[i]=tolower(t[i]);
+        }
+        if(strcmp(t,w) == 0) sum++;
+    }
+    printf("%d\n", sum);
+    return 0;
 }
