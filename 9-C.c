8c5ed93f6ebc6fdc881f6a6291de6d53f1c8a28f
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-02 09:08:46 +0900

    a

--- 9-C.c
+++ 9-C.c
@@ -4,5 +4,23 @@
 
 int main()
 {
-    
+    int n,P1score=0,P2score=0;
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        char temp1[101],temp2[101];
+        scanf("%s %s",temp1,temp2);
+        if(strcmp(temp1,temp2)==0){
+            P1score=P1score+1;
+            P2score=P2score+1;
+        }
+        else if(strcmp(temp1,temp2)>0){
+            P1score=P1score+3;
+        }
+        else if(strcmp(temp1,temp2)<0){
+            P2score=P2score+3;
+        }
+    }
+    printf("%d %d\n",P1score,P2score);
+    return 0;
 }
