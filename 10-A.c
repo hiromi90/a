70af1e231fe011db78920f0bd4ec70c2376b5f80
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-02 09:50:40 +0900

    a

--- 10-A.c
+++ 10-A.c
@@ -4,5 +4,10 @@
 
 int main()
 {
-    
+    double x1,x2,y1,y2;
+    double range=0.0;
+    scanf("%lf %lf %lf %lf",&x1,&y1,&x2,&y2);
+    range=sqrt(pow(x2-x1,2.0)+pow(y2-y1,2.0));
+    printf("%.10f\n",range);
+    return 0;
 }
