d88b36fff5e89a537966788bd7e37328da0fdfd8
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-02 10:06:43 +0900

    a

--- 10-B.c
+++ 10-B.c
@@ -4,5 +4,19 @@
 
 int main()
 {
-    
+    double a,b;
+    int C,c;
+    double rad_x;
+    scanf("%lf %lf %d",&a,&b,&C);
+    rad_x =C*M_PI/180;
+    double S;
+    S=a*b*sin(rad_x)/2.0;
+    double L;
+    double h;
+    h=b*sin(rad_x);
+    L=a+b+sqrt(a*a+b*b-2*a*b*cos(rad_x));
+    printf("%.10f\n",S);
+    printf("%.10f\n",L);
+    printf("%.10f\n",h);
+    return 0;
 }
