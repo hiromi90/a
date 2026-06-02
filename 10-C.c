ff0cffaeafe4e3c65f88ae1904e05ff21ecc9ed0
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-02 10:20:26 +0900

    a

--- 10-C.c
+++ 10-C.c
@@ -4,5 +4,24 @@
 
 int main()
 {
-    
+    int n;
+    while(1){
+        scanf("%d",&n);
+        if(n==0) break;
+        int s[n];
+        int sum=0;
+        double a2=0.0;
+        for(int i=0;i<n;i++){
+            scanf("%d",&s[i]);
+            sum += s[i];
+        }
+        double average = (double)sum/n;
+        for(int i=0;i<n;i++){
+            a2=a2+(s[i]-average)*(s[i]-average);
+        }
+        a2=a2/n;
+        double a=0.0;
+        a=sqrt(a2);
+        printf("%.10f\n",a);
+    }
 }
