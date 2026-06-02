9cd18c5f3cf3fb5f02ad643b6ec3d5f211aeed64
hiromi90 <x24104xx@aitech.ac.jp>
2026-06-02 09:41:49 +0900

    a

--- 9-D.c
+++ 9-D.c
@@ -4,5 +4,41 @@
 
 int main()
 {
-    
+    char str[1001];
+    char temp[1001];
+    scanf("%s",str);
+    strcpy(temp,str);
+    int q;
+    scanf("%d",&q);
+    int a,b;
+    int p=0;
+    for(int i=0;i<q;i++){
+        char order[10];
+        scanf("%s",order);
+        if(strcmp(order,"print")==0){
+            scanf("%d %d",&a,&b);
+            for(int j=a;j<=b;j++){
+                printf("%c",str[j]);
+            }
+            printf("\n");
+        }
+        else if(strcmp(order,"reverse")==0){
+            scanf("%d %d",&a,&b);
+            for(int j=a;j<=b;j++){
+                temp[j]=str[b-(j-a)];
+            }
+            strcpy(str,temp);
+        }
+        else if(strcmp(order,"replace")==0){
+            scanf("%d %d",&a,&b);
+            p=b-a+1;
+            char c[p+1];
+            scanf(" %s",c);
+            for(int j=a;j<=b;j++){
+                str[j]=c[j-a];
+            }
+            strcpy(temp, str);
+        }
+    }
+    return 0;
 }
